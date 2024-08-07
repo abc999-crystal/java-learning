94ffdefd1c9988308e4ee659ab0fb50056ed7f86
zdd <1846834504@qq.com>
2024-08-07 16:36:43 +0800

    feat：springboot 跨域配置

---
+++ src/main/java/springboot/cors/CorsConfig.java
@@ -0,0 +1,31 @@
+package springboot.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Auther zdd
+ * @Date 2024/08/07 16:35
+ * @Version v1.0.0
+ * @Description 跨域配置，允许所有跨域请求；这里配置了nginx就不用配了。
+ */
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
